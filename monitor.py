1dd094e37db9655e2b01061e3e0ecddcd408bdd3
Fernando FeX <fernandofigaroli@gmail.com>
2013-12-19 17:09:59 +0100

    prova

--- monitor.py
+++ monitor.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
-
+# Programma che usa la libreria MyOpen per andare in modalita monitor
+# attende comando dal gateway
+# se view lo stampa a video
+# se db lo scrive nel database sqlite
+# utilizza il db per leggere l'ultimo stato
+# e registrare se e cambiato
 import MyOpen
 from time import time
-S=MyOpen.Gateway("192.168.1.7",20000,"monitor")
 
+view=True
+db=True
+dbfile="monitor.db"
+
+gateway=MyOpen.Gateway("192.168.1.7",20000,"monitor")
+database=MyOpen.Db(dbfile)
 n=1
-print "Litening:..."
-par=MyOpen.Parser()
+print "In ascolto:..."
+parser=MyOpen.Parser()
 while True:
-    x=S.readcmd()
+    x=gateway.readcmd()
     print x,
-    print par.parsing(x)
+    print parser.parsing(x)
