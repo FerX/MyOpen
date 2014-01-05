8e5c095bb3771b1db76b473c34524c373a1b37de
Fernando FeX <fernandofigaroli@gmail.com>
2014-01-05 12:18:39 +0100

    errore e invio multiplo

--- sendcmd.py
+++ sendcmd.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
-
-#script da sistemare con lettura impostazioni da file config
+#Programma semplice per inviare manualmente comandi OpenWebNet al gateway
+#legge le impostazioni del gateway dal file di config.cfg
+#esempio uso:
+#./sendcmd.py "*1*1*77##"
 
 import MyOpen
 import sys
 
-print "ricorda di passare comando myopen racchiuso tra virgolette"
+#leggi impostazioni da config e le mette in un dizionario
+conf=MyOpen.ReadConfig("config.cfg","MyDaemon").read()
 
-X=MyOpen.Gateway("192.168.1.13",20000)
+#Connessione al gataway
+gateway=MyOpen.Gateway(conf["gateway"],int(conf["port"]))
 
-if len(sys.argv)>1:
-    cmd=sys.argv[1]
-    print cmd
-    print X.sendcmd(cmd)
+if len(sys.argv)==1:
+    #nessun argomento passato
+    print """sendcmd.py - programma per inviare comandi al gateway
+             uso: ./sendcmd.py comandoOpenWebNet
+             si possono passare anche piu comandi simultaneamente
+             es: ./sendcmd.py *1*1*77## *1*1*76## """
+    sys.exit()
+
+for cmd in sys.argv[1:]:
+    print "Invio: "+cmd
+    print "Risposta: "+gateway.sendcmd(cmd)
 
